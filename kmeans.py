e7922395af82682ddbd1551f1ce475f91a8b6352
fdas3213 <szx9404@gmail.com>
2019-03-28 12:19:42 -0700

    created KMeans object

--- kmeans.py
+++ kmeans.py
@@ -1,56 +1,57 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn.datasets import load_iris
 from numpy.linalg import norm
 
-def load_data():
-	iris = datasets.load_iris()
-	x = iris['data']
-	y = iris['target']
-	shuffle_indices = np.random.permutation(len(x))
-	return x[shuffle_indices], y[shuffle_indices]
+class KMeans:
+	#implement kmeans 
+	def __init__(self, num_clusters):
+		self.n_cluster = num_clusters
 
-def l2_dist(x,y):
-	return norm(x-y, axis = 1)
+	def l2_dist(self, x, y):
+		#use euclidean distance
+		return norm(x-y, axis = 1)
 
-def initCenter(data, num_cluster):
-	numSamples, dim = data.shape
-	return data[np.random.randint(0, numSamples, num_cluster)]
+	def initCenter(self, x):
+		#initialize class centers
+		N, D =x.shape
+		return x[np.random.randint(0, N, self.n_cluster)]
 
-def k_means(num_cluster, data):
-	numSamples, dim = data.shape
-	clusters = np.zeros(numSamples)
-	nonstop = True
-	#step 1: initialize cluster centers
-	init_means = initCenter(data, num_cluster)
+	def fit(self, x):
+		N, D = x.shape
+		self.clusters = np.zeros(N)
+		nonstop = True
+		#step 1: initialize cluster centers
+		self.init_means = self.initCenter(x)
+		#step 2: iteratively update class center
+		while nonstop:
+			nonstop = False
+			for i in range(N):
+				dist = self.l2_dist(x[i], self.init_means)
+				cluster_idx = np.argmin(dist)
+				if self.clusters[i] != cluster_idx:
+					nonstop = True
+					self.clusters[i] = cluster_idx
+			#update cluster centroids
+			for k in range(self.n_cluster):
+				self.init_means[k] = np.mean(x[self.clusters == k], axis = 0)
 
-	while nonstop:
-		nonstop = False
-		#for each sample
-		for i in range(numSamples):
-			#find the closest centroid
-			dist = l2_dist(data[i], init_means)
-			cluster_idx = np.argmin(dist)
-			if clusters[i] != cluster_idx:
-				nonstop = True
-				clusters[i] = cluster_idx
+	def draw(self, data):
+		plt.scatter(data[self.clusters == 0, 0], data[self.clusters == 0, 1], c = 'red')
+		plt.scatter(data[self.clusters == 1, 0], data[self.clusters == 1, 1], c = 'blue')
+		plt.scatter(data[self.clusters == 2, 0], data[self.clusters == 2, 1], c = 'green')
+		plt.scatter(self.init_means[:,0], self.init_means[:,1], c = 'yellow')
+		plt.legend(['cluster_0', 'cluster_1', 'cluster_2', 'center'])
+		plt.show()
 
-		#update centroids
-		for k in range(num_cluster):
-			init_means[k] = np.mean(data[clusters == k], axis = 0)
-			
-	return clusters, init_means
-
-def draw(num_cluster, data):
-	clusters, mean = k_means(num_cluster, data)
-	plt.scatter(data[clusters == 0, 0], data[clusters == 0, 1], c = 'red')
-	plt.scatter(data[clusters == 1, 0], data[clusters == 1, 1], c = 'blue')
-	plt.scatter(data[clusters == 2, 0], data[clusters == 2, 1], c = 'green')
-	plt.scatter(mean[:,0], mean[:,1], c = 'yellow')
-	plt.show()
-	
 if __name__ == "__main__":
-	x, y = load_data()
-	draw(3, x)
-	#c, m = k_means(3,x)
+	#load data and shuffle indices
+	x = load_iris()['data']
+	shuffle_indices = np.random.permutation(len(x))
+	x = x[shuffle_indices]
+	#fit KMeans model
+	clf = KMeans(3)
+	clf.fit(x)
+	#draw the plot
+	clf.draw(x)
 
